e771e8c2e19c7848e57bc5be664c352562388da2
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2025-03-29 19:07:01 -0600

    Added the code to make data sending work

--- data_sender.py
+++ data_sender.py
@@ -1,34 +1,101 @@
+import datetime as dt
 import abc
-from typing import TypeVar
+import json
+import time
+import requests
+from typing import TypeVar, Generic
+import asyncio
 
-T = TypeVar["T"]
+class DataReading:
+    @abc.abstractmethod
+    def to_tuple(self) -> tuple:
+        '''Creates a tuple with the components of the reading'''
+        pass
 
-class DataSender():
-    def __init__(self, readings_file: str):
+    @abc.abstractmethod
+    def to_json(self) -> dict:
+        '''Creates a tuple with the components of the reading'''
+        pass
+
+    def serialize(self) -> str:
+        return ",".join(map(lambda x: f"{x:.2f}",self.to_tuple())) + "\n"
+
+T = TypeVar("T", bound=DataReading)
+class DataSender(Generic[T]):
+    def __init__(self, readings_file: str, api_endpoint: str, sending_interval: int, reading_interval: int):
+        '''
+        @param sending_interval The number of minutes required to send data summaries to the database
+        @param readint_interval The number of seconds to use to make sensor readings
+        '''
         self.readings_file = readings_file
+        self.api_endpoint = api_endpoint
+        self.time_of_last_summary = None
+        self.sending_interval = sending_interval
+        self.reading_interval = reading_interval
 
     @abc.abstractmethod
-    def read_now(self) -> T:
+    def read_data(self) -> T:
+        '''Reads actual data from the sensors'''
         pass
 
     @abc.abstractmethod
-    def reading_to_tuple(self, reading: T) -> tuple:
-        pass
-
-    @abc.abstractmethod
-    def reading_to_json(self, reading: T) -> dict:
-        pass
-
-    @abc.abstractmethod
-    def serialize_reading(self, reading: T) -> str:
+    def make_reading(self, *args) -> T:
+        '''Creates a reading with the given arguments; i. e., a reading object constructor'''
         pass
 
     @abc.abstractmethod
     def deserialize_reading(self, string: str) -> T:
-        pass
-    
+        '''
+        Parses a reading from a given string; it splits the string using a comma as a separator
+        and converts every component to a float, then it passes the components to the
+        `make_reading' method
+        '''
+        components = tuple(map(lambda x: float(x), string.split(",")))
+        return self.make_reading(*components)
     
     def save_reading(self, reading: T):
         with open(self.readings_file, "a") as readings_file:
-            readings_file.write(str(reading))
+            readings_file.write(reading.serialize())
 
+    def create_summary(self) -> T:
+        with open(self.readings_file, "r") as readings_file:
+            readings = [ self.deserialize_reading(reading) for reading in readings_file.readlines() ]
+            count = len(readings)
+
+            components = list(zip(*[ reading.to_tuple() for reading in readings ]))
+            components = [ sum(component_list) / count for component_list in components ]
+            summary = self.make_reading(*components)
+            # readings_file.truncate(0)
+
+            return summary
+    
+    async def send_summary(self):
+        reading = self.create_summary()
+        json_payload = reading.to_json()
+        json_payload["timestamp"] = str(dt.datetime.now())
+
+        response = requests.post(
+            self.api_endpoint,
+            json = json_payload
+        )
+
+        if not response.ok:
+            json_response = json.loads(response.content.decode())
+            if json_response["error"] == "Unactive device":
+                raise BaseException("The device has been deactivated")
+            else:
+                print(json_response)
+        else:
+            print(f"{str(dt.datetime.now())}. The summary was successfully sent")
+
+    async def main(self):
+        while True:
+            current_time = dt.datetime.now()
+            if current_time.minute % self.sending_interval == 0 and self.time_of_last_summary != current_time:
+                self.time_of_last_summary = current_time
+                await self.send_summary()
+            self.save_reading(self.read_data())
+            await asyncio.sleep(self.reading_interval)
+    
+    def _main(self):
+        asyncio.run(self.main())
--- main.py
+++ main.py
@@ -1,115 +1,99 @@
 #! venv/Scripts/python.exe
 
+from data_sender import DataSender, DataReading
+from random import uniform, random
 import asyncio
-import datetime as dt
-import random
-import time
-import requests
-import json
+from multiprocessing import Process
 
 API_ENDPOINT = "http://localhost:3000/api"
-EXTERNAL_READINGS_LOCATION = "./readings.csv"
+EXTERNAL_READINGS_FILE = "./external-readings.csv"
+INTERNAL_READINGS_FILE = "./internal-readings.csv"
 KEY_FILE_LOCATION = "./key.txt"
 TOKEN_FILE_LOCATION = "./token.txt"
 
 API_ENDPOINTS = {
     "LOAD_EXTERNAL_READING": f"{API_ENDPOINT}/load-external-reading",
+    "LOAD_INTERNAL_READING": f"{API_ENDPOINT}/load-internal-reading",
 }
 
-class ExternalReading():
-    def __init__(self, timestamp: dt.datetime, temperature: float, light: float, current: float, voltage: float, wattage: float) -> None:
-        self.timestamp = timestamp
+class ExternalReading(DataReading):
+    def __init__(self, temperature: float, light: float, current: float, voltage: float, wattage: float):
+        super().__init__()
         self.temperature = temperature
         self.light = light
         self.current = current
         self.voltage = voltage
         self.wattage = wattage
-    
-    def __str__(self):
-        return f"{str(self.timestamp)},{self.temperature:.2f},{self.light:.2f},{self.current:.2f},{self.voltage:.2f},{self.wattage:.2f}\n"
-
-    @staticmethod
-    def make_now(temperature: float, light: float, current: float, voltage: float, wattage: float) -> "ExternalReading":
-        return ExternalReading(dt.datetime.now(), temperature, light, current, voltage, wattage)
-    
-    @staticmethod
-    def deserialize(representation: str) -> "ExternalReading":
-        timestamp, *everything_else = representation.split(",")
-        timestamp = dt.datetime.fromisoformat(timestamp)
-        everything_else = [ float(component) for component in everything_else ]
-        return ExternalReading(timestamp, *everything_else)
 
     def to_tuple(self):
-        return (
-            self.timestamp,
-            self.temperature,
-            self.light,
-            self.current,
-            self.voltage,
-            self.wattage
-        )
-
-    def to_json_dict(self):
+        return ( self.temperature, self.light, self.current, self.voltage, self.wattage )
+    
+    def to_json(self):
         return {
-            "timestamp": str(self.timestamp),
             "temperature": self.temperature,
             "light": self.light,
             "current": self.current,
             "voltage": self.voltage,
-            "wattage": self.wattage
+            "wattage": self.wattage,
         }
 
-def read_external_data() -> ExternalReading:
-    '''This should be implemented later'''
-    return ExternalReading(
-        dt.datetime.now(), 
-        random.uniform(0, 32),
-        random.randint(0, 50),
-        random.uniform(0, 100),
-        random.uniform(0, 100),
-        random.uniform(0, 100),
-    )
+class InternalReading(DataReading):
+    def __init__(self, temperature: float, humidity: float):
+        super().__init__()
+        self.temperature = temperature
+        self.humidity = humidity
 
-def save_reading(reading: ExternalReading):
-    with open(EXTERNAL_READINGS_LOCATION, "a") as readings_file:
-        readings_file.write(str(reading))
+    def to_tuple(self):
+        return ( self.temperature, self.humidity )
+    
+    def to_json(self):
+        return {
+            "temperature": self.temperature,
+            "humidity": self.humidity
+        }
 
+class ExternalSender(DataSender[ExternalReading]):
+    def __init__(self):
+        super().__init__(EXTERNAL_READINGS_FILE, API_ENDPOINTS["LOAD_EXTERNAL_READING"], 10, 5)
+    
+    def make_reading(self, *args):
+        return ExternalReading(*args)
 
-TIME_OF_LAST_SUMMARY = None
+    def read_data(self):
+        return ExternalReading(
+            uniform(0, 32),
+            uniform(0, 1000),
+            uniform(0, 100),
+            uniform(0, 100),
+            uniform(0, 1000)
+        )
 
-def create_external_summary():
-    with open(EXTERNAL_READINGS_LOCATION, "r") as readings_file:
-        readings = [ ExternalReading.deserialize(reading) for reading in readings_file.readlines() ]
-        count = len(readings)
-        _, *usable_components = list(zip(*[ reading.to_tuple() for reading in readings ]))
-        print(usable_components)
-        usable_components = [ sum(component_list) / count for component_list in usable_components ]
-        summary = ExternalReading.make_now(*usable_components)
-        readings_file.truncate(0)
-        return summary
-
-async def send_external_summary(reading: ExternalReading):
-    response = requests.post(
-        API_ENDPOINTS["LOAD_EXTERNAL_READING"],
-        json = reading.to_json_dict()
-    )
-    if not response.ok:
-        json_response = json.loads(response.content.decode())
-        if json_response["error"] == "Unactive device":
-            raise BaseException("The device has been deactivated")
-    else:
-        print(f"{str(dt.datetime.now())}. The summary was successfully sent")
-
-def main():
-    while True:
-        current_time = dt.datetime.now()
-        if current_time.minute % 10 == 0 and TIME_OF_LAST_SUMMARY != current_time:
-            summary = create_external_summary()
-            global TIME_OF_LAST_SUMMARY
-            TIME_OF_LAST_SUMMARY = current_time
-            send_external_summary(summary)
-        save_reading(read_external_data())
-        time.sleep(5)
+class InternalSender(DataSender[InternalReading]):
+    def __init__(self):
+        super().__init__(INTERNAL_READINGS_FILE, API_ENDPOINTS["LOAD_INTERNAL_READING"], 5, 3)
+    
+    def make_reading(self, *args):
+        return InternalReading(*args)
+    
+    def read_data(self):
+        return InternalReading(uniform(0, 32), random())
 
 if __name__ == '__main__':
-    main()
+    external_sender = ExternalSender()
+    internal_sender = InternalSender()
+
+    first_process = Process(target = external_sender._main)
+    second_process = Process(target = internal_sender._main)
+    
+    try:
+        first_process.start()
+        second_process.start()
+        first_process.join()
+        second_process.join()
+
+    except Exception:
+        print("Shutting down processes")
+
+    finally:
+        first_process.terminate()
+        second_process.terminate()
